6d96dc77d47cd6ec4ae5199fbdff181e61eee40e
Ehsan Taheri <ehsan.taheri1400@gmail.com>
2024-11-11 16:12:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,35 @@ import numpy as np
 import nibabel as nib
 import cv2
 from PIL import Image
+import keras.backend as K
 
+# Dice Coefficient Metric
 def dice_coef(y_true, y_pred, smooth=1):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
     return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+# Precision Metric
+def precision(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision_value = true_positives / (predicted_positives + K.epsilon())
+    return precision_value
+
+# Sensitivity Metric (also known as recall)
+def sensitivity(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    sensitivity_value = true_positives / (possible_positives + K.epsilon())
+    return sensitivity_value
+
+# Specificity Metric
+def specificity(y_true, y_pred):
+    true_negatives = K.sum(K.round(K.clip((1 - y_true) * (1 - y_pred), 0, 1)))
+    possible_negatives = K.sum(K.round(K.clip(1 - y_true, 0, 1)))
+    specificity_value = true_negatives / (possible_negatives + K.epsilon())
+    return specificity_value
 
 
 # Load model
