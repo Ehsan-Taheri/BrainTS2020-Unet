4b797c7747bdf1ce2a0d6336a3fed7c275034f25
Ehsan Taheri <ehsan.taheri1400@gmail.com>
2024-11-11 16:10:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,13 @@ import nibabel as nib
 import cv2
 from PIL import Image
 
+def dice_coef(y_true, y_pred, smooth=1):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
 # Load model
 model = tf.keras.models.load_model("/Users/ciro/Downloads/trained_brain_mri_model.h5", 
                                    custom_objects={
